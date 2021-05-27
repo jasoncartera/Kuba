68886e4cdc02ba0620af82b6e07271d6ed814e3d
Jason Carter <carterjasona@gmail.com>
2021-05-26 19:40:33 -0700

    #TODO print score and marble number #TODO reset game after win #TODO reset game if user wants to

--- graphics.py
+++ graphics.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+from pygame.locals import *
 from KubaGame import KubaGame, Player
 
 ## COLORS ##
@@ -10,20 +12,119 @@ BLACK = (0, 0, 0)
 class Game:
     """
     Main control for KubaGame
+
+    :param playerw is the white marble player
+    :param playerb is the black marble player
     """
-    def __init__(self, playera, playerb):
-        self._game = KubaGame(('Jason', 'W'), ('Sunny', 'B'))
+    def __init__(self, playerw, playerb):
+        self._game = KubaGame(playerw, playerb)
+        self._playerw = playerw
+        self._playerb = playerb
+        self._board = self._game.board
         self._graphics = Graphics()
+        self._marble_target = None
+        self._marble_dir = None
 
     @property
     def game(self):
+        """
+        Game property
+        :return: self._game
+        """
         return self._game
 
-    def event_loop(self):
-        """"""
+    @property
+    def board(self):
+        """
+        Board property
+        :return: self._board
+        """
+        return self._board
+
+    @property
+    def marble_target(self):
+        """
+        marble_target property
+        :return: self._marble_target
+        """
+        return self._marble_target
+
+    @marble_target.setter
+    def marble_target(self, target):
+        self._marble_target = target
+
+    @property
+    def marble_dir(self):
+        """
+        marble direction property
+        :return: self._marble_dir
+        """
+        return self._marble_dir
+
+    @marble_dir.setter
+    def marble_dir(self, dir):
+        self._marble_dir = dir
+
+    def setup(self):
+        """
+        Sets up the board game
+        """
+        self._graphics.set_up_window()
+        self._graphics.draw_board_squares()
+        self._graphics.draw_marbles(self.board)
+
+    def game_loop(self):
+        """
+        Implements the game logic and events triggered and updates the board
+        """
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.marble_target = self._graphics.board_coords(pygame.mouse.get_pos())
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.marble_dir = self._graphics.board_coords(pygame.mouse.get_pos())
+                if self.game.get_current_turn() is None:
+                    current_player = self._playerw
+                elif self.game.player_a.is_turn:
+                    current_player = self._playerw
+                else:
+                    current_player = self._playerb
+
+                if self.marble_dir[1] - self.marble_target[1] > 0:
+                    self.game.make_move(current_player[0], self.marble_target, 'R')
+                elif self.marble_dir[1] - self.marble_target[1] < 0:
+                    self.game.make_move(current_player[0], self.marble_target, 'L')
+                elif self.marble_dir[0] < self.marble_target[0]:
+                    self.game.make_move(current_player[0], self.marble_target, 'F')
+                elif self.marble_dir[0] > self.marble_target[0]:
+                    self.game.make_move(current_player[0], self.marble_target, 'B')
+
+    def update(self):
+        """
+        Updates the graphics
+        """
+        self._graphics.update_display(self.board)
+
+    def main(self):
+        """
+        Executes the game
+        """
+        self.setup()
+
+        #TODO will run the main loop
+        # while True:
+        #     self.event_loop()
+        #     self.update()
 
 class Graphics:
-    """ Graphics for the game """
+    """
+    Graphics for the game
+    """
     def __init__(self):
         self._width = 602
         self._height = 700
@@ -33,6 +134,22 @@ class Graphics:
         self._caption = "Kuba Game"
         self._screen = pygame.display.set_mode((self._width,self._height))
         self._background = self._screen.fill(GRAY)
+
+    @property
+    def screen(self):
+        """
+        Screen property
+        :return: self._screen
+        """
+        return self._screen
+
+    @property
+    def marble_size(self):
+        """
+        Marble size property
+        :return: self._marble_size
+        """
+        return self._marble_size
 
     def set_up_window(self):
         """
@@ -54,6 +171,8 @@ class Graphics:
         """
         Updates the display board
         """
+
+        self.screen.fill(GRAY)
         self.draw_board_squares()
         self.draw_marbles(board)
         pygame.display.update()
@@ -65,28 +184,31 @@ class Graphics:
         for i in range(7):
             for j in range(7):
                 if board.board[i][j] != None:
-                    pygame.draw.circle(self._screen, board.board[i][j].rgb, (i * self._marble_size + self._marble_size / 2, j * self._marble_size + self._marble_size / 2),
+                    pygame.draw.circle(self._screen, board.board[i][j].rgb, (j * self._marble_size + self._marble_size / 2, i * self._marble_size + self._marble_size / 2),
                                        self._marble_size / 2.5)
+
+    def board_coords(self, px):
+        """
+        Converts a tuple of pixel coords to coords on board
+        :return square the mouse is on
+        """
+        row, col = px
+        return (col // self._marble_size, row // self._marble_size)
+
 
     def display_score(self, game):
         pass
 
 def main():
-    graphic = Graphics()
     game = Game(('Jason', 'W'),('Sunny', 'B'))
+    game.setup()
     clock = pygame.time.Clock()
     while True:
         clock.tick(60)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                break
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+        game.game_loop()
 
-        graphic.update_display(game.game.board)
-
-    pygame.quit()
+        game.update()
 
 if __name__ == '__main__':
     main()
